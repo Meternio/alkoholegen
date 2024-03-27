8c748aaab2e3c6aa9d4f3442b0987687edeaad27
Florian Hitz <florian@raffiniert.biz>
2024-03-27 10:38:09 +0100

    finished login

--- app/login.js
+++ app/login.js
@@ -7,6 +7,7 @@ import {
   Text,
   View,
   ScrollView,
+  Animated,
 } from "react-native";
 import { Button, TextInput, Snackbar } from "react-native-paper";
 import logo from "../assets/icon.png";
@@ -19,10 +20,30 @@ import {
 
 export default function LoginScreen() {
   const [mode, setMode] = useState("login"); // ["login", "signup", "forgot"]
+  const [fadeAnim, setFadeAnim] = useState(new Animated.Value(1));
   const [notification, setNotification] = useState(null);
   const [loading, setLoading] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const changeMode = (mode) => {
+    // Fade out
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => {
+      // After the fade out is done, set the mode
+      setMode(mode);
+
+      // Then fade in
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    });
+  };
 
   const handleSubmit = async () => {
     setLoading(true);
@@ -39,7 +60,7 @@ export default function LoginScreen() {
     try {
       await signInWithEmailAndPassword(auth, username, password);
     } catch (error) {
-      setNotification(error.message + " Invalid username or password");
+      setNotification(error.message + " Invalid email or password");
     } finally {
       setLoading(false);
     }
@@ -49,7 +70,7 @@ export default function LoginScreen() {
     try {
       await createUserWithEmailAndPassword(auth, username, password);
     } catch (error) {
-      setNotification(error.message + " Invalid username or password");
+      setNotification(error.message + " Invalid email or password");
     } finally {
       setLoading(false);
     }
@@ -59,7 +80,7 @@ export default function LoginScreen() {
     try {
       await sendPasswordResetEmail(auth, username, password);
     } catch (error) {
-      setNotification(error.message + " Invalid username");
+      setNotification(error.message + " Invalid email");
     } finally {
       setLoading(false);
     }
@@ -75,79 +96,89 @@ export default function LoginScreen() {
           style={styles.scrollView}
         >
           <Image source={logo} style={styles.image} resizeMode="contain" />
-          <Text style={styles.title}>
-            {
+          <Animated.View style={{ ...styles.container, opacity: fadeAnim }}>
+            <Text style={styles.title}>
               {
-                login: "Login",
-                signup: "Sign Up",
-                forgot: "Forgot Password",
-              }[mode]
-            }
-          </Text>
-          <View style={styles.inputView}>
-            <TextInput
-              label="Email"
-              value={username}
-              onChangeText={setUsername}
-              onSubmitEditing={handleLogin}
-              mode="outlined"
-              style={styles.input}
-            />
-            {mode !== "forgot" && (
+                {
+                  login: "Login",
+                  signup: "Sign Up",
+                  forgot: "Forgot Password",
+                }[mode]
+              }
+            </Text>
+            <View style={styles.inputView}>
               <TextInput
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
+                label="Email"
+                value={username}
+                onChangeText={setUsername}
                 onSubmitEditing={handleLogin}
                 mode="outlined"
-                secureTextEntry
                 style={styles.input}
               />
-            )}
-          </View>
+              {mode !== "forgot" && (
+                <TextInput
+                  label="Password"
+                  value={password}
+                  onChangeText={setPassword}
+                  onSubmitEditing={handleLogin}
+                  mode="outlined"
+                  secureTextEntry
+                  style={styles.input}
+                />
+              )}
+            </View>
 
-          <View style={styles.buttonView}>
-            <Button
-              mode="contained"
-              onPress={handleSubmit}
-              loading={loading}
-              disabled={loading}
-              icon="login"
-            >
-              {mode === "login"
-                ? "Login"
-                : mode === "signup"
-                ? "Sign Up"
-                : "Reset Password"}
-            </Button>
-          </View>
+            <View style={styles.buttonView}>
+              <Button
+                mode="contained"
+                onPress={handleSubmit}
+                loading={loading}
+                disabled={loading}
+                icon={
+                  {
+                    login: "login",
+                    signup: "account-plus-outline",
+                    forgot: "lock-alert-outline",
+                  }[mode]
+                }
+              >
+                {
+                  {
+                    login: "Login",
+                    signup: "Sign Up",
+                    forgot: "Reset Password",
+                  }[mode]
+                }
+              </Button>
+            </View>
 
-          <View style={styles.otherOptionsView}>
-            {mode !== "signup" ? (
-              <Text style={styles.footerText}>
-                Don't Have Account?
-                <Pressable onPress={() => setMode("signup")}>
-                  <Text style={styles.signup}> Sign Up</Text>
+            <View style={styles.otherOptionsView}>
+              {mode !== "signup" ? (
+                <Text style={styles.footerText}>
+                  Don't Have Account?
+                  <Pressable onPress={() => changeMode("signup")}>
+                    <Text style={styles.signup}> Sign Up</Text>
+                  </Pressable>
+                </Text>
+              ) : (
+                <Text style={styles.footerText}>
+                  Already Have Account?
+                  <Pressable onPress={() => changeMode("login")}>
+                    <Text style={styles.signup}> Login</Text>
+                  </Pressable>
+                </Text>
+              )}
+              {mode !== "forgot" ? (
+                <Pressable onPress={() => changeMode("forgot")}>
+                  <Text style={styles.forgetText}>Forgot Password?</Text>
                 </Pressable>
-              </Text>
-            ) : (
-              <Text style={styles.footerText}>
-                Already Have Account?
-                <Pressable onPress={() => setMode("login")}>
-                  <Text style={styles.signup}> Login</Text>
+              ) : (
+                <Pressable onPress={() => changeMode("login")}>
+                  <Text style={styles.forgetText}>Back to Login</Text>
                 </Pressable>
-              </Text>
-            )}
-            {mode !== "forgot" ? (
-              <Pressable onPress={() => setMode("forgot")}>
-                <Text style={styles.forgetText}>Forgot Password?</Text>
-              </Pressable>
-            ) : (
-              <Pressable onPress={() => setMode("login")}>
-                <Text style={styles.forgetText}>Back to Login</Text>
-              </Pressable>
-            )}
-          </View>
+              )}
+            </View>
+          </Animated.View>
         </ScrollView>
       </SafeAreaView>
       <Snackbar
